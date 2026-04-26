8b12d8728d725d588501b3d0838e11668a64a997
Tdahuyou <dahuyoutop@gmail.com>
2026-04-26 12:31:33 +0800

    📝 Update notes - 2026-04-26 12:31:33

---
+++ notes/0023. Vue 简介/assets/2.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello Vue!')
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="count++">点击次数：{{ count }}</button>
+</template>
